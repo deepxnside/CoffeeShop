ddd98c31bd57953465391bc8aac38710c8bd5b24
day <day@deeznuts.com>
2022-04-30 22:26:15 +0545

    quest 1.5

--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -1,6 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var deafultConnString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+var assembly = typeof(Program).Assembly.GetName().Name;
+
+builder.Services.AddIdentityServer().AddConfigurationStore(opts =>
+    {
+        opts.ConfigureDbContext = b =>
+
+            b.UseNpgsql(deafultConnString,
+                opt => opt.MigrationsAssembly(assembly));
+
+    }
+).AddOperationalStore(opts =>
+    {
+        opts.ConfigureDbContext = b =>
+
+            b.UseNpgsql(deafultConnString,
+                opt => opt.MigrationsAssembly(assembly));
+
+    }
+
+).AddDeveloperSigningCredential();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
 app.Run();
